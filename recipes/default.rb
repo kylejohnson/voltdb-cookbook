582fe328d731917721120e3b21440aceef126bb7
Kyle Johnson <kjohnson@gnulnx.net>
2014-06-17 16:26:11 -0400

    Use the default client and admin ports

--- recipes/default.rb
+++ recipes/default.rb
@@ -74,6 +74,6 @@ end
 bash "Start VoltDB" do
   user "root"
   cwd "/media/voltdb"
-  code "/opt/voltdb-ent-4.4/bin/voltdb create /media/voltdb/catalog.jar -B --deployment=/media/voltdb/deployment.xml --client=21212 --admin=21211 --host=#{voltdb_master_ip}"
+  code "/opt/voltdb-ent-4.4/bin/voltdb create /media/voltdb/catalog.jar -B --deployment=/media/voltdb/deployment.xml --host=#{voltdb_master_ip}"
   not_if 'jps | grep VoltDB'
 end
